6d7efede00d7fc79355d5f6ae3f8003ca43a09d3
shashwat28 <shashwat3770@gmail.com>
2020-09-15 15:30:17 +0530

    Modified code
    - added new processed directory for face cutouts
    - extracting face cutouts to processed directory

--- src/face_detection/face_detection.py
+++ src/face_detection/face_detection.py
@@ -14,7 +14,7 @@ def scaled_image(img):
     return faces
 
 
-def extract_bounded_faces(img, faces):
+def extract_bounded_faces(img, faces, FACE_CUTOUT_DIR):
     '''cropping out the faces from the subject image using coordinates from
     the list returned of the scaled_image method'''
     name_counter = 0
@@ -22,26 +22,29 @@ def extract_bounded_faces(img, faces):
         cv2.rectangle(img, (center_x, center_y),
                       (center_x + width, center_y+height),
                       (255, 0, 0), 2)
-        roi_color = img[center_y:center_y + height, center_x:center_x + width]
-        cv2.imshow('img{}'.format(name_counter), roi_color)
+        face_cutout = img[center_y:center_y + height,
+                          center_x:center_x + width]
+        cv2.imwrite(os.path.join(FACE_CUTOUT_DIR,
+                                 'img{}.jpg'.format(name_counter)),
+                    face_cutout)
         name_counter = name_counter+1
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
 
     # giving path to the data source directory
+    PROJECT_PATH = "./../../"
     MODEL_PATH = "model/face_detection/haarcascade_frontalface_default.xml"
-    IMAGE_DIR = "data/raw/images"
+    IMAGE_DIR = PROJECT_PATH + "data/raw/images"
+    FACE_CUTOUT_DIR = PROJECT_PATH + "data/processed/images"
 
     # loading the haar cascade pretrained classifier
-    face_cascade = cv2.CascadeClassifier(MODEL_PATH)
+    face_cascade = cv2.CascadeClassifier(PROJECT_PATH + MODEL_PATH)
 
     # iterate through the names of contents of the folder
     for image_path in os.listdir(IMAGE_DIR):
-        print(image_path)
         input_path = os.path.join(IMAGE_DIR, image_path)
         image_to_scale = cv2.imread(input_path)
         scaled_img = scaled_image(image_to_scale)
-        extract_bounded_faces(image_to_scale, scaled_img)
+        extract_bounded_faces(image_to_scale, scaled_img, FACE_CUTOUT_DIR)
